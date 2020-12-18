b2ae8d77574e546a268169776f064011e840a401
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2020-12-18 16:33:55 +0000

    Arrays

---
+++ Fundamentals/03.Arrays/Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] arr = new int[number];
+            int sum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];
+            }
+
+            Console.WriteLine(string.Join(" ",arr));
+            //Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}

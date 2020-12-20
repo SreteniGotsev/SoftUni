cc5e69a4662267d5746b5874fef2b2bf0df82f4e
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2020-12-20 13:41:37 +0000

    Lists

---
+++ Fundamentals/05.Lists/CardsGame/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> handOne = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToList();
+
+            List<int> handTwo = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToList();
+
+            int currentCardPlayerOne = 0;
+            int currentCardPlayerTwo = 0;
+
+            string winner = string.Empty;
+            while (handOne.Count != 0 && handTwo.Count != 0)
+            {
+                currentCardPlayerOne = handOne[0];
+                currentCardPlayerTwo = handTwo[0];
+                winner = Clash(currentCardPlayerOne, currentCardPlayerTwo);
+
+                if (winner == "playerOne")
+                {
+                    handOne.Add(currentCardPlayerOne);
+                    handOne.Add(currentCardPlayerTwo);
+                    handOne.RemoveAt(0);
+                    handTwo.RemoveAt(0);
+
+                }
+                else if (winner == "playerTwo")
+                {
+                    handTwo.Add(currentCardPlayerTwo);
+                    handTwo.Add(currentCardPlayerOne);
+                    handTwo.RemoveAt(0);
+                    handOne.RemoveAt(0);
+                }
+                else if (winner == "noOne")
+                {
+                    handOne.RemoveAt(0);
+                    handTwo.RemoveAt(0);
+                }
+            }
+
+            string playerWinner = string.Empty;
+            int sum = 0;
+            List<int> hand = new List<int>();
+
+            if (handTwo.Count == 0)
+            {
+                playerWinner = "First";
+                hand = handOne;
+            }
+            else if (handOne.Count == 0)
+            {
+                playerWinner = "Second";
+                hand = handTwo;
+            }
+
+            for (int i = 0; i < hand.Count; i++)
+            {
+                sum += hand[i];
+            }
+
+            Console.WriteLine($"{playerWinner} player wins! Sum: {sum}");
+        }
+
+        private static string Clash(int cardP1, int cardp2)
+        {
+            string winner = string.Empty;
+
+            if (cardP1 > cardp2)
+            {
+                winner = "playerOne";
+            }
+            else if (cardP1 < cardp2)
+            {
+                winner = "playerTwo";
+            }
+            else if (cardP1 == cardp2)
+            {
+                winner = "noOne";
+            }
+
+            return winner;
+        }
+    }
+}
---
+++ Fundamentals/05.Lists/HouseParty/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            List<string> currentInput = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                currentInput = Console.ReadLine()                                      
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .ToList();
+                if (currentInput[2] == "going!")
+                {
+                    if (names.Contains(currentInput[0]))
+                    {
+                        Console.WriteLine($"{currentInput[0]} is already in the list!");
+                    }
+                    else
+                    {
+                    names.Add(currentInput[0]);
+                    }
+
+                }
+                else if (currentInput.Contains("not"))
+                {
+                    if (names.Contains(currentInput[0]))
+                    {
+                        names.Remove(currentInput[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{currentInput[0]} is not in the list!");
+                    }
+                }
+            }
+            for (int i = 0; i < names.Count; i++)
+            {
+            Console.WriteLine(names[i]);
+
+            }
+        }
+    }
+}

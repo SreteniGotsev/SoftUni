21fb76a4d09a658d7eb7395d2de7d5adb502442e
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2020-12-21 20:32:33 +0000

    Object&Classes

---
+++ Fundamentals/06.ObjectsAndClasses/AdvertisementMessage/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            string[] phrases = { "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product." };
+
+            string[] events = { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] names = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"{phrases[rnd.Next(0, phrases.Length)]}" +
+                    $" {events[rnd.Next(0, events.Length)]} " +
+                    $"{names[rnd.Next(0, names.Length)]}  " +
+                    $"{cities[rnd.Next(0, cities.Length)]} ");
+            }
+        }
+    }
+}

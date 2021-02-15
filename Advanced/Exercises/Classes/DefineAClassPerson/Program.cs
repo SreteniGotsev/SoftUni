95a8d02e5a0f11efb9801c49b21eef26c314fc96
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2021-02-15 23:40:17 +0200

    Classes

---
+++ Advanced/Exercises/Classes/DefineAClassPerson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DefiningClasses;
+
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.Name = "Gosho";
+            personOne.Age = 22;
+
+            Person personTwo = new Person()
+            {
+                Name = "Misho",
+                Age = 34
+            };
+
+            Console.WriteLine(personOne.Name);
+            Console.WriteLine(personTwo.Age);
+
+        }
+    }
+}
